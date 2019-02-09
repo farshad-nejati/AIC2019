1ddfd618bcfd0aac5527c66ae7aaadcf88719bf6
farshad-nejati <nejati.farshad93@gmail.com>
2019-02-09 13:59:29 +0330

    first commit

---
+++ src/client/AI.java
@@ -0,0 +1,36 @@
+package client;
+
+import client.model.Direction;
+import client.model.Hero;
+import client.model.HeroName;
+import client.model.World;
+
+import java.util.Random;
+
+public class AI
+{
+
+    public void preProcess(World world)
+    {
+        //System.out.println("pre process started");
+
+    }
+
+    public void pickTurn(World world)
+    {
+        //System.out.println("pick started");
+
+    }
+
+    public void moveTurn(World world)
+    {
+        //System.out.println("move started");
+
+    }
+
+    public void actionTurn(World world) {
+        //System.out.println("action started");
+
+    }
+
+}
