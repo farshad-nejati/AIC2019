25c7d3f25e2baec0b76572dd10350cf5c06c949f
farshad <nejati.farshad93@gmail.com>
2019-02-24 08:07:04 +0330

    distance 3 added to InZoneMoving with bug

--- src/client/NewAI/move/inZone/InZoneMoving.java
+++ src/client/NewAI/move/inZone/InZoneMoving.java
@@ -15,6 +15,7 @@ public class InZoneMoving {
     private int phaseNumber;
     private ArrayList<HeroNeighbors> heroNeighbors = new ArrayList<>();
     private ArrayList<Cell> blockedCells;
+    int maxAreaEffect = 5;
 
     public InZoneMoving(ArrayList<Hero> inZoneHeroes, World world) {
         this.myHeroes = inZoneHeroes;
@@ -31,31 +32,33 @@ public class InZoneMoving {
     public void move(World world, ArrayList<Cell> blockedCellsNoneZoneHeroes) {
         for (Hero myHero : myHeroes) {
             addBlockedCells(myHeroes,myHero, blockedCellsNoneZoneHeroes);
-            Cell bestCell = findBestCellToMove(myHero);
-            if (bestCell != null) {
-                Direction[] directions = world.getPathMoveDirections(myHero.getCurrentCell(), bestCell);
-                new Printer().printDirections(directions);
-                if (directions.length > 0) {
+            Direction bestDirection = findBestCellToMove(myHero);
+            if (bestDirection != null) {
+//                Direction[] directions = world.getPathMoveDirections(myHero.getCurrentCell(), );
+//                new Printer().printDirections(directions);
+//                if (directions.length > 0) {
                     System.out.println("Hero Move " + myHero.getName() + myHero.getId() + " in ");
-                    world.moveHero(myHero, directions[0]);
+//                    world.moveHero(myHero, directions[0]);
+                    world.moveHero(myHero, bestDirection);
                     System.out.println("Hero Move successfully");
-                }
+//                }
             } else {
                 System.out.println("\n\nBest Cell not Found! \n");
             }
         }
     }
 
-    private Cell findBestCellToMove(Hero myHero) {
+    private Direction findBestCellToMove(Hero myHero) {
         HeroNeighbors myHeroNeighbors = HeroNeighbors.findByHero(this.heroNeighbors, myHero);
         ArrayList<Cell> neighbors = myHeroNeighbors.getNeighborCells();
         ArrayList<ObjectiveCellThreat> sortedThreatObjects = new ArrayList<>();
         Collections.sort(this.objectiveCellThreats, ObjectiveCellThreat.threatNumberComparator);
         int minNumber = 10;
-        for (ObjectiveCellThreat objectThreat : this.objectiveCellThreats) {
+        ArrayList<ObjectiveCellThreat> objectThreatCellThree = getObjectsWithThreeDistance(myHero, this.objectiveCellThreats);
+        for (ObjectiveCellThreat objectThreat : objectThreatCellThree) {
             Cell cell = objectThreat.getCell();
-            if (!neighbors.contains(cell))
-                continue;
+//            if (!neighbors.contains(cell))
+//                continue;
 
             int threatNumber = objectThreat.getThreatNumber();
             if (threatNumber < minNumber) {
@@ -68,12 +71,79 @@ public class InZoneMoving {
         Collections.sort(sortedThreatObjects, ObjectiveCellThreat.threatHPComparator);
         for (ObjectiveCellThreat obj : sortedThreatObjects) {
             Cell cell = obj.getCell();
-            if (!blockedCells.contains(cell)) {
-                return cell;
-            }
+//            if (!blockedCells.contains(cell) && !myHero.getCurrentCell().equals(cell)) {
+            ArrayList<Cell> newBlockedCells = new ArrayList<>();
+            return recursiveFindDirection(myHero.getCurrentCell(), cell, newBlockedCells);
+//            Direction returnDirection = recursiveFindDirection(myHero.getCurrentCell(), cell, newBlockedCells);
+//            if (returnDirection != null) {
+//                return returnDirection;
+//            }
+//                return cell;
+//            }
 //            ObjectiveCellThreat bestThreatObject = sortedThreatObjects.get(0);
         }
         return null;
+    }
+
+    private ArrayList<ObjectiveCellThreat> getObjectsWithThreeDistance(Hero currentHero, ArrayList<ObjectiveCellThreat> objectiveCellThreats) {
+        ArrayList<ObjectiveCellThreat> returnObject = new ArrayList<>();
+
+        ArrayList<Hero> currentMyHeroes = new ArrayList<>(this.myHeroes);
+        currentMyHeroes.remove(currentHero);
+
+        int maxEffect = this.maxAreaEffect;
+        while (returnObject.size() == 0 && maxEffect > this.maxAreaEffect - 2) {
+
+            for (ObjectiveCellThreat threatObject : objectiveCellThreats) {
+                boolean flag = true;
+                for (Hero hero : currentMyHeroes) {
+                    int distance = world.manhattanDistance(threatObject.getCell(), hero.getCurrentCell());
+                    if (distance < maxEffect) {
+                        flag = false;
+                    }
+                }
+                if (flag) {
+                    returnObject.add(threatObject);
+                }
+                for (ObjectiveCellThreat obj : returnObject) {
+                    int distance = world.manhattanDistance(obj.getCell(), threatObject.getCell());
+                }
+            }
+            maxEffect--;
+        }
+        return returnObject;
+    }
+
+    public Direction recursiveFindDirection(Cell currentCell, Cell targetCell, ArrayList<Cell> blockCells) {
+        ArrayList<Cell> copyOfBlockedCells = new ArrayList<>(blockCells);
+        Direction[] directions;
+        if (copyOfBlockedCells.size() > 0) {
+            directions = world.getPathMoveDirections(currentCell, targetCell, copyOfBlockedCells);
+        } else {
+            directions = world.getPathMoveDirections(currentCell, targetCell);
+        }
+        if (directions.length > 0) {
+            Direction direction = directions[0];
+            Cell newCell = findCellWithDirection(currentCell, direction);
+            if (newCell.isInObjectiveZone()) {
+                return direction;
+            } else {
+                copyOfBlockedCells.add(newCell);
+                return recursiveFindDirection(currentCell, targetCell, copyOfBlockedCells);
+            }
+        }
+        return null;
+    }
+
+    private Cell findCellWithDirection(Cell currentCell, Direction direction) {
+        if (direction.equals(Direction.DOWN))
+            return world.getMap().getCell(currentCell.getRow() + 1, currentCell.getColumn());
+        else if (direction.equals(Direction.UP))
+            return world.getMap().getCell(currentCell.getRow() - 1, currentCell.getColumn());
+        else if (direction.equals(Direction.RIGHT))
+            return world.getMap().getCell(currentCell.getRow(), currentCell.getColumn() + 1);
+        else
+            return world.getMap().getCell(currentCell.getRow(), currentCell.getColumn() - 1);
     }
 
     private void addBlockedCells(ArrayList<Hero> myHeroes, Hero currentHero, ArrayList<Cell> blockedCellsNoneZoneHeroes) {
@@ -101,9 +171,9 @@ public class InZoneMoving {
 
     private Ability findDangerousAbility(Ability[] offensiveAbilities) {
         Ability dangerousAbility = null;
-        int abilityPower = 0;
+        int abilityPower = 1000;
         for (Ability ability : offensiveAbilities) {
-            if (ability.isReady() && ability.getPower() > abilityPower) {
+            if (ability.getPower() < abilityPower) { // return min power
                 dangerousAbility = ability;
             }
         }
