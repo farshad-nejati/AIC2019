3c9761c46d564890ded57d030db39a2d1ccee886
mehdi <mehdi.sh4rifi96@gmail.com>
2019-03-01 17:17:38 +0330

    change method name to reduceDistanceWithOppHeroesInObjectiveZone

--- src/client/IntelligentAI/MinMaxMove.java
+++ src/client/IntelligentAI/MinMaxMove.java
@@ -116,7 +116,7 @@ class MinMaxMove {
 
 //        score += ScoreStrategy.hitByOppHeroes(myHero, myHeroDirection, virtualWorld, copyOfMyHeroesMove, copyOfOppHeroesMove);
 //        score += ScoreStrategy.otherMyHeroCell(myHero, myHeroDirection, oppHero, virtualWorld, copyOfMyHeroesMove);
-        score += ScoreStrategy.moveTowardToOppHeroes(myHero, myHeroDirection,otherOurHeroes, oppHero, oppHeroes, virtualWorld, copyOfMyHeroesMove, copyOfOppHeroesMove, blocks);
+        score += ScoreStrategy.reduceDistanceWithOppHeroesInObjectiveZone(myHero, myHeroDirection,otherOurHeroes, oppHero, oppHeroes, virtualWorld, copyOfMyHeroesMove, copyOfOppHeroesMove, blocks);
 
 
         return score;
