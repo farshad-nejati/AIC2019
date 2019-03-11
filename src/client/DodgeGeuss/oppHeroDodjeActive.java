1ae93971e10de2bafa2c0bb437d5bf41dd9f93ff
farshidn <nfarshidn@gmail.com>
2019-03-12 02:13:08 +0330

    add class for dodge

---
+++ src/client/DodgeGeuss/oppHeroDodjeActive.java
@@ -0,0 +1,12 @@
+package client.DodgeGeuss;
+
+import client.model.Hero;
+
+import java.util.ArrayList;
+
+public class oppHeroDodjeActive {
+   public static ArrayList<DodjeDescriber> oppHeroJodjeActiveGeuss(ArrayList<Hero> oppHeroes){
+       ArrayList<DodjeDescriber> dodjeDescribers= new ArrayList<>();
+       return dodjeDescribers;
+   }
+}
